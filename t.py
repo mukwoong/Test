d30f8d1975f6d85d50189c98f07e8a32750187d6
unknown <nanasher445@gmail.com>
2024-05-09 18:00:07 +0800

    add recoginze

--- t.py
+++ t.py
@@ -8,6 +8,18 @@ def fill_blank_cells(df):
     return df
 
 df = pd.read_excel("0all-sku.xlsx")
+# 提前处理空白项目
 df = fill_blank_cells(df)
-subset = df.iloc[:, 0]
-print(subset[30: 40])
+
+# 示例1
+subset = df[df.iloc[:, 0] == "SU24215-1"]
+
+# 尺码(5-13列)
+size = subset.iloc[:, 5:14]
+# 价格(14列)
+price = subset.iloc[0, 14]
+# po号
+po = subset.iloc[:, -2]
+
+
+print(po)
